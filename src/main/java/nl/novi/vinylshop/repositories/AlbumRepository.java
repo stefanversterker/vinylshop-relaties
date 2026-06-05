008cf30046eaa95bc326dc8ec017334137d3c853
stefanversterker <stefanversterker@gmail.com>
2026-06-05 19:20:20 +0200

    functies toegevoegd aan repositories

--- src/main/java/nl/novi/vinylshop/repositories/AlbumRepository.java
+++ src/main/java/nl/novi/vinylshop/repositories/AlbumRepository.java
@@ -4,6 +4,14 @@ import nl.novi.vinylshop.entities.AlbumEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AlbumRepository  extends JpaRepository<AlbumEntity, Long> {
+
+    List<AlbumEntity> findByStockItemsNotEmpty();
+
+    List<AlbumEntity> findByStockItemsEmpty();
+
+    List<AlbumEntity> findByGenre_Id(Long genreId);
 }
